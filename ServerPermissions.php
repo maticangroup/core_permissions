266e2f3858f35f4dce44742e2acdec41e1caec14
Matican Matican <maticangroup@gmail.com>
2019-07-23 16:31:27 +0430

    hossein: adding all permissions

--- ServerPermissions.php
+++ ServerPermissions.php
@@ -157,4 +157,194 @@ class ServerPermissions
     const repository_weekdays_edit = 'repository_weekdays_edit';
     const repository_weekdays_new = 'repository_weekdays_new';
 
+    const accounting_coupongroup_all = 'accounting_coupongroup_all';
+    const accounting_coupongroup_fetch = 'accounting_coupongroup_fetch';
+    const accounting_coupongroup_confirm = 'accounting_coupongroup_confirm';
+    const accounting_coupongroup_stop = 'accounting_coupongroup_stop';
+    const accounting_coupongroup_delete = 'accounting_coupongroup_delete';
+    const accounting_coupongroup_edit = 'accounting_coupongroup_edit';
+    const accounting_coupongroup_new = 'accounting_coupongroup_new';
+    const accounting_coupongroup_add_allowed_customer = 'accounting_coupongroup_add_allowed_customer';
+    const accounting_coupongroup_remove_allowed_customer = 'accounting_coupongroup_remove_allowed_customer';
+    const accounting_coupongroup_add_usage = 'accounting_coupongroup_add_usage';
+
+    const accounting_giftcardgroup_all = 'accounting_giftcardgroup_all';
+    const accounting_giftcardgroup_fetch = 'accounting_giftcardgroup_fetch';
+    const accounting_giftcardgroup_new = 'accounting_giftcardgroup_new';
+    const accounting_giftcardgroup_confirm = 'accounting_giftcardgroup_confirm';
+    const accounting_giftcardgroup_stop = 'accounting_giftcardgroup_stop';
+    const accounting_giftcardgroup_add_usage = 'accounting_giftcardgroup_add_usage';
+    const accounting_giftcardgroup_set_card_status = 'accounting_giftcardgroup_set_card_status';
+
+    const accounting_invoice_all = 'accounting_invoice_all';
+    const accounting_invoice_fetch = 'accounting_invoice_fetch';
+    const accounting_invoice_new = 'accounting_invoice_new';
+    const accounting_invoice_add_invoice_item = 'accounting_invoice_add_invoice_item';
+    const accounting_invoice_remove_invoice_item = 'accounting_invoice_remove_invoice_item';
+    const accounting_invoice_finalize_invoice = 'accounting_invoice_finalize_invoice';
+    const accounting_invoice_rethink_invoice = 'accounting_invoice_rethink_invoice';
+    const accounting_invoice_get_all_invoice_groups = 'accounting_invoice_get_all_invoice_groups';
+
+    const accounting_paymentrequest_all = 'accounting_paymentrequest_all';
+    const accounting_paymentrequest_fetch = 'accounting_paymentrequest_fetch';
+    const accounting_paymentrequest_delete = 'accounting_paymentrequest_delete';
+    const accounting_paymentrequest_edit = 'accounting_paymentrequest_edit';
+    const accounting_paymentrequest_new = 'accounting_paymentrequest_new';
+    const accounting_paymentrequest_add_payment = 'accounting_paymentrequest_add_payment';
+    const accounting_paymentrequest_confirm_payment = 'accounting_paymentrequest_confirm_payment';
+    const accounting_paymentrequest_set_type = 'accounting_paymentrequest_set_type';
+    const accounting_paymentrequest_set_status = 'accounting_paymentrequest_set_status';
+    const accounting_paymentrequest_get_payment_methods = 'accounting_paymentrequest_get_payment_methods';
+
+    const authentication_client_all = 'authentication_client_all';
+    const authentication_client_fetch = 'authentication_client_fetch';
+    const authentication_client_new = 'authentication_client_new';
+    const authentication_client_update = 'authentication_client_update';
+
+    const authentication_permission_fetch = 'authentication_permission_fetch';
+    const authentication_permission_delete = 'authentication_permission_delete';
+    const authentication_permission_update = 'authentication_permission_update';
+    const authentication_permission_fetch_file = 'authentication_permission_fetch_file';
+
+    const authentication_permissiongroup_all = 'authentication_permissiongroup_all';
+
+    const authentication_role_new = 'authentication_role_new';
+    const authentication_role_update = 'authentication_role_update';
+    const authentication_role_fetch = 'authentication_role_fetch';
+    const authentication_role_all = 'authentication_role_all';
+    const authentication_role_grant_permission = 'authentication_role_grant_permission';
+    const authentication_role_deny_permission = 'authentication_role_deny_permission';
+    const authentication_role_get_roles_permissions = 'authentication_role_get_roles_permissions';
+
+    const authentication_user_all = 'authentication_user_all';
+    const authentication_user_set_role = 'authentication_user_set_role';
+    const authentication_user_login = 'authentication_user_login';
+    const authentication_user_logout = 'authentication_user_logout';
+    const authentication_user_block = 'authentication_user_block';
+    const authentication_user_send_password = 'authentication_user_send_password';
+    const authentication_user_send_password_to_user = 'authentication_user_send_password_to_user';
+    const authentication_user_check_status = 'authentication_user_check_status';
+
+    const crm_customergroup_new = 'crm_customergroup_new';
+    const crm_customergroup_all = 'crm_customergroup_all';
+    const crm_customergroup_fetch = 'crm_customergroup_fetch';
+    const crm_customergroup_add_person = 'crm_customergroup_add_person';
+    const crm_customergroup_remove_person = 'crm_customergroup_remove_person';
+    const crm_customergroup_update = 'crm_customergroup_update';
+    const crm_customergroup_set_status = 'crm_customergroup_set_status';
+
+    const delivery_deliverymethod_all = 'delivery_deliverymethod_all';
+    const delivery_deliverymethod_fetch = 'delivery_deliverymethod_fetch';
+    const delivery_deliverymethod_update = 'delivery_deliverymethod_update';
+    const delivery_deliverymethod_new = 'delivery_deliverymethod_new';
+    const delivery_deliverymethod_add_delivery_person = 'delivery_deliverymethod_add_delivery_person';
+    const delivery_deliverymethod_remove_delivery_person = 'delivery_deliverymethod_remove_delivery_person';
+    const delivery_deliverymethod_add_dispatch = 'delivery_deliverymethod_add_dispatch';
+    const delivery_deliverymethod_add_queue = 'delivery_deliverymethod_add_queue';
+    const delivery_deliverymethod_remove_queue = 'delivery_deliverymethod_remove_queue';
+    const delivery_deliverymethod_add_allowed_size = 'delivery_deliverymethod_add_allowed_size';
+    const delivery_deliverymethod_remove_allowed_size = 'delivery_deliverymethod_remove_allowed_size';
+    const delivery_deliverymethod_get_all_types = 'delivery_deliverymethod_get_all_types';
+    const delivery_deliverymethod_change_queue_status = 'delivery_deliverymethod_change_queue_status';
+    const delivery_deliverymethod_change_delivery_method_status = 'delivery_deliverymethod_change_delivery_method_status';
+    const delivery_deliverymethod_get_allowed_delivery_methods = 'delivery_deliverymethod_get_allowed_delivery_methods';
+    const delivery_deliverymethod_get_available_queues = 'delivery_deliverymethod_get_available_queues';
+
+    const delivery_deliveryperson_all = 'delivery_deliveryperson_all';
+    const delivery_deliveryperson_fetch = 'delivery_deliveryperson_fetch';
+    const delivery_deliveryperson_update = 'delivery_deliveryperson_update';
+    const delivery_deliveryperson_add_allowed_district = 'delivery_deliveryperson_add_allowed_district';
+    const delivery_deliveryperson_remove_allowed_district = 'delivery_deliveryperson_remove_allowed_district';
+    const delivery_deliveryperson_get_delivery_method_people = 'delivery_deliveryperson_get_delivery_method_people';
+    const delivery_deliveryperson_change_status = 'delivery_deliveryperson_change_status';
+
+    const delivery_dispatch_all = 'delivery_dispatch_all';
+    const delivery_dispatch_fetch = 'delivery_dispatch_fetch';
+    const delivery_dispatch_delete = 'delivery_dispatch_delete';
+    const delivery_dispatch_edit = 'delivery_dispatch_edit';
+    const delivery_dispatch_new = 'delivery_dispatch_new';
+    const delivery_dispatch_set_origin = 'delivery_dispatch_set_origin';
+    const delivery_dispatch_set_destination = 'delivery_dispatch_set_destination';
+    const delivery_dispatch_set_delivery_person = 'delivery_dispatch_set_delivery_person';
+    const delivery_dispatch_set_status = 'delivery_dispatch_set_status';
+    const delivery_dispatch_set_queue = 'delivery_dispatch_set_queue';
+    const delivery_dispatch_set_delivery_method = 'delivery_dispatch_set_delivery_method';
+    const delivery_dispatch_finalize_dispatch = 'delivery_dispatch_finalize_dispatch';
+    const delivery_dispatch_rethink_dispatch = 'delivery_dispatch_rethink_dispatch';
+    const delivery_dispatch_set_dispatch_queue = 'delivery_dispatch_set_dispatch_queue';
+    const delivery_dispatch_set_dispatch_location = 'delivery_dispatch_set_dispatch_location';
+    const delivery_dispatch_confirm_dispatch_payment = 'delivery_dispatch_confirm_dispatch_payment';
+
+    const inventory_inventory_all = 'inventory_inventory_all';
+    const inventory_inventory_new = 'inventory_inventory_new';
+    const inventory_inventory_fetch = 'inventory_inventory_fetch';
+    const inventory_inventory_add_phone = 'inventory_inventory_add_phone';
+    const inventory_inventory_remove_phone = 'inventory_inventory_remove_phone';
+    const inventory_inventory_update = 'inventory_inventory_update';
+    const inventory_inventory_change_status = 'inventory_inventory_change_status';
+    const inventory_inventory_all_item_products = 'inventory_inventory_all_item_products';
+    const inventory_inventory_inventory_all_item_products = 'inventory_inventory_inventory_all_item_products';
+    const inventory_inventory_get_inventory_item_products = 'inventory_inventory_get_inventory_item_products';
+    const inventory_inventory_get_inventory_products = 'inventory_inventory_get_inventory_products';
+    const inventory_inventory_get_item_products = 'inventory_inventory_get_item_products';
+
+    const inventory_shelve_new = 'inventory_shelve_new';
+    const inventory_shelve_fetch = 'inventory_shelve_fetch';
+    const inventory_shelve_add_phone = 'inventory_shelve_add_phone';
+    const inventory_shelve_remove_phone = 'inventory_shelve_remove_phone';
+    const inventory_shelve_all = 'inventory_shelve_all';
+    const inventory_shelve_update = 'inventory_shelve_update';
+    const inventory_shelve_change_status = 'inventory_shelve_change_status';
+    const inventory_shelve_shelve_all_item_products = 'inventory_shelve_shelve_all_item_products';
+    const inventory_shelve_get_shelve_item_products = 'inventory_shelve_get_shelve_item_products';
+    const inventory_shelve_get_shelve_products = 'inventory_shelve_get_shelve_products';
+    const inventory_shelve_get_shelves_products = 'inventory_shelve_get_shelves_products';
+
+    const inventory_transferdeed_all = 'inventory_transferdeed_all';
+    const inventory_transferdeed_fetch = 'inventory_transferdeed_fetch';
+    const inventory_transferdeed_delete = 'inventory_transferdeed_delete';
+    const inventory_transferdeed_update = 'inventory_transferdeed_update';
+    const inventory_transferdeed_new = 'inventory_transferdeed_new';
+    const inventory_transferdeed_import_to_inventory = 'inventory_transferdeed_import_to_inventory';
+    const inventory_transferdeed_add_product_to_deed = 'inventory_transferdeed_add_product_to_deed';
+    const inventory_transferdeed_remove_product_from_deed = 'inventory_transferdeed_remove_product_from_deed';
+    const inventory_transferdeed_confirm_deed = 'inventory_transferdeed_confirm_deed';
+    const inventory_transferdeed_transfer_deed = 'inventory_transferdeed_transfer_deed';
+
+    const sale_offergroup_all = 'sale_offergroup_all';
+    const sale_offergroup_fetch = 'sale_offergroup_fetch';
+    const sale_offergroup_new = 'sale_offergroup_new';
+    const sale_offergroup_update = 'sale_offergroup_update';
+    const sale_offergroup_add_product = 'sale_offergroup_add_product';
+    const sale_offergroup_remove_product = 'sale_offergroup_remove_product';
+    const sale_offergroup_change_status = 'sale_offergroup_change_status';
+
+    const sale_order_update = 'sale_order_update';
+    const sale_order_new = 'sale_order_new';
+    const sale_order_all = 'sale_order_all';
+    const sale_order_fetch = 'sale_order_fetch';
+    const sale_order_add_product = 'sale_order_add_product';
+    const sale_order_remove_product = 'sale_order_remove_product';
+    const sale_order_accept_order_list = 'sale_order_accept_order_list';
+    const sale_order_ignore_order_list = 'sale_order_ignore_order_list';
+    const sale_order_save_order = 'sale_order_save_order';
+    const sale_order_confirm_order = 'sale_order_confirm_order';
+    const sale_order_get_person_orders = 'sale_order_get_person_orders';
+
+    const sale_pricingdeed_all = 'sale_pricingdeed_all';
+    const sale_pricingdeed_fetch = 'sale_pricingdeed_fetch';
+    const sale_pricingdeed_fetch_confirmed = 'sale_pricingdeed_fetch_confirmed';
+    const sale_pricingdeed_delete = 'sale_pricingdeed_delete';
+    const sale_pricingdeed_update = 'sale_pricingdeed_update';
+    const sale_pricingdeed_new = 'sale_pricingdeed_new';
+    const sale_pricingdeed_accept_pricing_list = 'sale_pricingdeed_accept_pricing_list';
+    const sale_pricingdeed_ignore_pricing_list = 'sale_pricingdeed_ignore_pricing_list';
+    const sale_pricingdeed_save_pricing_list = 'sale_pricingdeed_save_pricing_list';
+    const sale_pricingdeed_add_product = 'sale_pricingdeed_add_product';
+    const sale_pricingdeed_remove_product = 'sale_pricingdeed_remove_product';
+    const sale_pricingdeed_add_offer_group_products = 'sale_pricingdeed_add_offer_group_products';
+    const sale_pricingdeed_add_shelve_products = 'sale_pricingdeed_add_shelve_products';
+    const sale_pricingdeed_add_pricing_deed_products = 'sale_pricingdeed_add_pricing_deed_products';
+    const sale_pricingdeed_apply_pricing_deed = 'sale_pricingdeed_apply_pricing_deed';
+
 }
