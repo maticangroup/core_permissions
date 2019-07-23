7f8e92d3bee1b0fe3d384e6d2e94f73fddc640ef
Matican Matican <maticangroup@gmail.com>
2019-07-23 15:49:37 +0430

    Hossein adding company permissions

---
+++ ServerPermissions.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hossein
+ * Date: 23/07/19
+ * Time: 15:37
+ */
+
+namespace App\Permissions;
+
+
+class ServerPermissions
+{
+    const repository_company_all = 'repository_company_all';
+    const repository_company_fetch = 'repository_company_fetch';
+    const repository_company_delete = 'repository_company_delete';
+    const repository_company_update = 'repository_company_update';
+    const repository_company_new = 'repository_company_new';
+    const repository_company_get_phones = 'repository_company_get_phones';
+    const repository_company_add_phone = 'repository_company_add_phone';
+    const repository_company_remove_phone = 'repository_company_remove_phone';
+    const repository_company_get_locations = 'repository_company_get_locations';
+    const repository_company_add_location = 'repository_company_add_location';
+    const repository_company_remove_location = 'repository_company_remove_location';
+    const repository_company_get_related_people = 'repository_company_get_related_people';
+    const repository_company_remove_related_person = 'repository_company_remove_related_person';
+    const repository_company_add_related_person = 'repository_company_add_related_person';
+    const repository_company_get_employees = 'repository_company_get_employees';
+    const repository_company_add_employee = 'repository_company_add_employee';
+    const repository_company_remove_employee = 'repository_company_remove_employee';
+    const repository_company_setcontainer = 'repository_company_setcontainer';
+    const repository_company_getsubscribedservices = 'repository_company_getsubscribedservices';
+}
